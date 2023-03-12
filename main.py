3444e25f29e9660d168d47776aa194c18467ad53
Ausaf Ahmed <ausaf100@gmail.com>
2023-03-12 00:09:56 -0500

    added basic game loop

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("welcome to the game")
+import sys
+ 
+import pygame
+from pygame.locals import *
+ 
+pygame.init()
+ 
+fps = 60
+fpsClock = pygame.time.Clock()
+
+width, height = 1280, 720
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Game')
+
+player = pygame.image.load('player.jpeg')
+player = pygame.transform.scale(player, (100, 100))
+
+def draw(): 
+    screen.blit(player, player.get_rect()) 
+
+def update(): 
+  pass
+
+# Game loop.
+while True:
+  screen.fill((0, 0, 0)) # Fill the screen with black.
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+  
+  # Update.
+  update()
+  # Draw.
+  draw()
+
+  pygame.display.flip()
+  fpsClock.tick(fps)
